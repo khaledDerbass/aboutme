35f3b800f1f2269a23aa56f568c81255db4aa349
Khaled <derbasskhaled1@gmail.com>
2021-05-27 06:20:33 +0300

    to push lab04

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# aboutme
+# About Me, with a guessing game
+### I build an simple webpage with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the webpage
 
-Khalid  was Navigator 
-Manal was Driver 
+## Author: Manal Ateyyat
+## License: MIT License
+## Built with: Java Script
+
+## How to use?
+### User schould ask 7 Yes/ No questions to learn about the site's owner "Me" so user can get to know me better..
+
+### Comments:  This is my first exposure to user stories
+
+
+### The navigator was Khaled and the driver was Manal
+
--- appjava/appjava.js
+++ appjava/appjava.js
@@ -1,4 +1,6 @@
 'use strict';
+
+
 function question1()
 {
   let marks=0;
@@ -29,23 +31,7 @@ function question1()
   }
 question1();
 
-//correctAnswer(userVisit);
-/*
-if (userVisit.toUpperCase() == 'Y' || userVisit.toUpperCase() == 'YES')
-{
 
-    alert("Thanks for answering");
-}
-else
-{
-        while (!(userVisit.toUpperCase() == 'Y' || userVisit.toUpperCase() == 'YES'))
-        {
-          alert("Please type your answer with ** (Y/YES)** options ONLY");
-          userVisit=prompt('Do you think that I love programming ?');
-        }
-        alert("CONGRATS, Thanks for answering");
-}
-*/
 function question2()
 {let type = prompt('Do you know what i studied before ?');
 console.log("Please answer with only (N/NO)");
@@ -73,29 +59,10 @@ switch (type) {
   }
   question2();
 
-/*
-//correctAnswer(type);
-if (type.toUpperCase() == 'NO' || type.toUpperCase() == 'N' || type.toUpperCase() == 'YES' || type.toUpperCase() == 'NO')
-{
-
-    console.log("Thanks for answering");
-    alert("Thanks for answering");
-  
-}
-else
-{
-        while (!(type.toUpperCase() == 'Y' || type.toUpperCase() == 'N' || type.toUpperCase() == 'YES' || type.toUpperCase() == 'NO'))
-        {
-          alert("Please type your answer with ** (y/n) or (yes/no) ** options ONLY");
-          type=prompt('Do you have a shop or personal used ?');
-        }
-        console.log("Thanks for answering");
-        alert("CONGRATS, Thanks for answering");
-
-}*/
 
 function question3()
-{let branch=prompt('Do you think that i\'m in love with the HomeMade food ?');
+{
+  let branch=prompt('Do you think that i\'m in love with the HomeMade food ?');
 branch=branch.toUpperCase();
 console.log("Please answer with only (N/NO)");
 
@@ -126,23 +93,7 @@ switch (branch) {
   question3();
 
 
-/*if (branch.toUpperCase() == 'Y' || branch.toUpperCase() == 'N' || branch.toUpperCase() == 'YES' || branch.toUpperCase() == 'NO')
-{
 
-    console.log("Thanks for answering");
-    alert("Thanks for answering");
-}
-else
-{
-        while (!(branch.toUpperCase() == 'Y' || branch.toUpperCase() == 'N' || branch.toUpperCase() == 'YES' || branch.toUpperCase() == 'NO'))
-        {
-          alert("Please type your answer with ** (y/n) or (yes/no) ** options ONLY");
-          branch=prompt('Do you have another branches ?');
-
-        }
-        console.log("Thanks for answering");
-        alert("CONGRATS, Thanks for answering");
-}*/
 
 function question4()
 {let feed=prompt('Do you know my Full Name ?');
@@ -174,29 +125,10 @@ switch (feed) {
 }
 question4();
 
-//correctAnswer(feed);      
 
-/*
-if (feed.toUpperCase() == 'Y' || feed.toUpperCase() == 'N' || feed.toUpperCase() == 'YES' || feed.toUpperCase() == 'NO')
-{
-
-    console.log("Thanks for answering");
-    alert("Thanks for answering");
-}
-else
-{
-        while (!(feed.toUpperCase() == 'Y' || feed.toUpperCase() == 'N' || feed.toUpperCase() == 'YES' || feed.toUpperCase() == 'NO'))
-        {
-          alert("Please type your answer with ** (y/n) or (yes/no) ** options ONLY");
-          feed=prompt('Do you like the letiants in our products ?');
-
-        }
-        console.log("Thanks for answering");
-        alert("CONGRATS, Thanks for answering");
-}
-*/
 function question5()
-{let feedback=prompt('Do you think that i love swimming');
+{
+  let feedback=prompt('Do you think that i love swimming');
 console.log("Please answer with only (N/NO)");
 feedback=feedback.toUpperCase();
 
@@ -220,14 +152,17 @@ switch (feedback) {
     {
       console.log("Please adhere to the answers found in the CONSOLE (N/NO)");
       break;}
-  
-}
+    }  
+
 }
 question5();
 
-
-
-/////////////////////////////////
+function seriesTop (){
+  let TopTen=['1- The Godfather Part I. ','2- The Godfather Part II. ','3- Dog Day Afternoon. ','4- Serpico.  ','5- The Panic in Needle Park. ','6- Carlito’s Way.  ','7- The Irishman. ','8- Scarface. ','9- Glengarry Glen Ross.  ','10- Scarecrow.' ];
+  for (let x=0;x<TopTen.length;x++)
+     document.write(TopTen[x]+"          ");  
+  }
+seriesTop ();  
 function question6()
 {
   let flag=0;
@@ -235,7 +170,8 @@ function question6()
   console.log("The number is 5");
   
   
-  for (let x=1;x<=4 && flag == 0;x++){
+  for (let x=1;x<=4 && flag == 0;x++)
+  {
     if (guessNum == 5)
       {
         alert('Thanks for answering correctly');
@@ -243,151 +179,92 @@ function question6()
         marks++;
       }
     else if (guessNum >= 0 && guessNum <5)
-      {
         guessNum=prompt("Too low, Try again please.");
-      }
+      
             
         
-    else if (guessNum > 5 && guessNum <=10){
+    else if (guessNum > 5 && guessNum <=10)
       guessNum=prompt("Too High, Try again please. ");
-    }
+    
       
     
     else
-      {
         guessNum=prompt("Please adhere to the RANGE of numbers {1,2,3....10} ");
-      }
+      
   }
   console.log("The number is 5 with "+x+" number of attempts");
   
-  }
-  question6();
-  
-///////////////////////////////////
+}
+question6();
 
-function ask7 ()
+question7 ();
+ 
+function question7 ()
 {
   let flag2=0;
-  //  let guess_Num2 = ['shirt', 'jeans', 'jacket', 'cap', 'short', 't-shirt','hoodie'];
-  let guess_Num2=[1,2,3,4,5,6,7,8,9,10];
-    console.log("The items you can choose are Jeans OR Shirt OR hoodie");
-    for (let i = 0; i < guess_Num2.length; i++) 
+  let guessNum2=[1,2,3,4,5,6,7,8,9,10];
+  
+  console.log("The numbers you can choose are 1 OR 5 OR 10");
+  
+  for (let j=0; j<guessNum2.length ;j++)
+  {
+    let Guess=prompt('Q7 : Guess a random number between 1-10');
+    
+    for (let x=0; x<guessNum2.length; x++)
     {
-        let userAns = prompt('Q7 : Can you guess my favorite item?');
-      //  userAns = userAns.toLowerCase();
-
-        for (let j = 0; j < guess_Num2.length; j++){
-            if (userAns == guess_Num2[j]) {
-                alert('Thanks for answering correctly');
-                marks++;
-                flag2=1;
-                break;
-            }
-        }
-        if (flag2 != 1)
-        alert('Sorry, wrong answer. try again');
-          
+      //if (Guess == guessNum2[x]) 
+      if ( Guess == 1 || Guess == 5 || Guess == 10)
+      {
+        alert('Congrats you got it right!');
+        flag2=1;
+        marks++;
+        break;          
+      }
     }
-     
+       if (flag2 != 1)
+      {
+        alert('Sorry, wrong answer. try again');
+      }
+    
+  
+  }
+ 
 }
-ask7 ();
+
+
 
 
 
 alert(' You got ' + marks + ' correct answers!');
 
-
-let TopTen=['1- The Godfather Part I. ','2- The Godfather Part II. ','3- Dog Day Afternoon. ','4- Serpico.  ','5- The Panic in Needle Park. ','6- Carlito’s Way.  ','7- The Irishman. ','8- Scarface. ','9- Glengarry Glen Ross.  ','10- Scarecrow.' ];
-for (let x=0;x<TopTen.length;x++)
-{
-  document.write(TopTen[x]+"          ");  
-}
-
-
-
-
-
-  /*let flag2=0;
-  let guessNum2=[1,2,3,4,5,6,7,8,9,10];
-  
-  console.log("The numbers you can choose are 1 OR 5 OR 10");
-  
-  for (let j=0; j<6 ;j++)
-  {
-    let Guess=prompt('Guess a random number between 1-10');
-    
-    for (let x=0; x<guessNum2.length; x++)
-    {
-      
-      if ( Guess == 1 || Guess == 5 || Guess == 10)
-      {
-        alert('Congrats you got it right!');
-        j=6;
-        marks++;
-        break;          
-      }
-    }
-       if (j !== 6)
-      {
-        alert('Sorry, wrong answer. try again');
-      }
-    
-  
-  }
+//  let guess_Num2 = ['shirt', 'jeans', 'jacket', 'cap', 'short', 't-shirt','hoodie'];
+  //  console.log("The items you can choose are Jeans OR Shirt OR hoodie");
+   
 
 
 /*
-function correctAnswer(UserAns)
+function RateMe()
 {
+  
+  let stars = prompt(userName+' Give us rating 1-5, please !');
+  if (stars >= 5)
+  stars = 5;
 
-  if (UserAns.toUpperCase() == 'Y' || UserAns.toUpperCase() == 'N')
-      {
-        console.log("Thanks for answer");
-        count++;
-      }
-      else
-      {
-        alert("Please answer the following Questions **(Y/N)** ONLY");
-        while (!(UserAns.toUpperCase() == 'Y' || UserAns.toUpperCase() == 'N')){      
-  switch (count) {
-        case 1:{
-          UserAns=prompt('Is it the first time you visit us ?'); count++;
-           break;}
-    
-        case 2:{
-          UserAns = prompt('Do you have a shop or personal used ?'); count++;
-           break;}
-        case 3:{
-          UserAns=prompt('Do you have another branches ?'); count++;
-            break;}
-        case 4:{
-          UserAns=prompt('Do you like the letiants in our products ?'); count++;
-          break;}
-        case 4:{
-          UserAns=prompt('Do you have any comment to improve our business ?'); count++;
-          break;}    
-        default:{
-          console.log("Thanks for answer");
-            break;}
-    }
-  }}
-}
+  let starResults='';
+
+  for (let i = 0; i < stars; i++) 
+  starResults += '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDvvIxXy4-x8TSIlWn-znOZirHvrsErjpxQw&usqp=CAU" width="50px">';
+
+
+let starResults=['The Godfather Part I','The Godfather Part II','Dog Day Afternoon','Serpico','The Panic in Needle Park','Carlito’s Way','The Irishman','Scarface','Glengarry Glen Ross','Scarecrow' ];
+
+  return starResults;
+}*/
+
  
 
-function RateMe()
-{
-  /*
-  let stars = prompt(userName+' Give us rating 1-5, please !');
-  if (stars >= 5)
-  stars = 5;
-
-  let starResults='';
-
-  for (let i = 0; i < stars; i++) 
-  starResults += '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDvvIxXy4-x8TSIlWn-znOZirHvrsErjpxQw&usqp=CAU" width="50px">';
 
 
-let starResults=['The Godfather Part I','The Godfather Part II','Dog Day Afternoon','Serpico','The Panic in Needle Park','Carlito’s Way','The Irishman','Scarface','Glengarry Glen Ross','Scarecrow' ];
-
-  return starResults;
-}*/
+  
+  
+ 
